553676244c83ad9b307efea1b6e7df5fd2a26763
MAXX <kumarmax402@gmail.com>
2021-01-07 01:16:04 +0530

    second commmit

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
 
+//define routes
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
